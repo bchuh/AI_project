ea4c920014ea7502b8db2e64479054d267a0c5a6
bchuh <zhuzengliangll@163.com>
2022-04-12 18:17:55 +0800

    改用记录坐标查重

--- load.py
+++ load.py
@@ -16,24 +16,32 @@ view.showMaximized()
 nodes_dir = os.path.join(os.getcwd(), 'nodes')
 folder_dirs = glob.glob(nodes_dir + "\*")
 folder_dirs.reverse()
+#Debug:
+combo_dict = {}
+#
+i=1
 for dir in folder_dirs:
     if ".py" in dir:
         continue
     _node: Node = load_node(dir, with_suffix_and_absolute_path=True)
     encoding = _node.encodeMatrix()
+    if encoding not in combo_dict:
+        combo_dict[encoding] = 1
+    else:
+        print("collide!!")
     _node.paint(scene)
     view.show()
     view.setBackgroundBrush(Qt.gray)
     view.update()
-    dieTime = QTime.currentTime().addMSecs(1000)
+    dieTime = QTime.currentTime().addMSecs(800)
     while (QTime.currentTime() < dieTime):
         QCoreApplication.processEvents(QEventLoop.AllEvents, 20)
     _node.clearPoly(scene)
     scene.clear()  # not working for some reason
     view.update()
-    dieTime = QTime.currentTime().addMSecs(500)
+    dieTime = QTime.currentTime().addMSecs(200)
     while (QTime.currentTime() < dieTime):
         QCoreApplication.processEvents(QEventLoop.AllEvents, 20)
     #view.hide()
-
+    i+=1
 app.exec_()
